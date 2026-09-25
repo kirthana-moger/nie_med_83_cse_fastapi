83b739a98fd201dea94b6a6f71227feecf945e35
Kirthana Shivappa Moger <2025cs_kirthanashivappamoger_c@nie.ac.in>
2026-09-25 15:14:19 +0530

    Create App.jsx

---
+++ apps/tickets_app/client/src/App.jsx
@@ -0,0 +1,21 @@
+import Login from "./Login";
+
+function Navigation() {
+    
+    return (
+       <>
+       
+       </>
+    );
+}
+
+function App() {
+    return (
+        <>
+        <Login/>
+       </>   
+    );
+}
+
+export default App;
+
